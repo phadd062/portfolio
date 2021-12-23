51f58a3e1746e14f538f8a7ac58b2744e1676889
Peter Haddad <phadd062@uottawa.ca>
2021-12-23 00:20:19 -0500

    updates

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,38 +1,47 @@
 <script setup lang="ts"></script>
 
 <template>
-	<div class="grid grid-cols-4 gap-3 p-3 bg-gray-50">
-		<div class="bg-white p-3 rounded-md col-span-4">1</div>
-		<div class="bg-white p-3 rounded-md">2</div>
-		<div class="bg-white p-3 rounded-md">3</div>
-		<div class="bg-white p-3 rounded-md">4</div>
-		<div class="bg-white p-3 rounded-md">5</div>
-		<div class="bg-white p-3 rounded-md row-span-4">6</div>
-		<div class="bg-white p-3 rounded-md">7</div>
-		<div class="bg-white p-3 rounded-md">8</div>
-		<div class="bg-white p-3 rounded-md">9</div>
-		<div class="bg-white p-3 rounded-md">2</div>
-		<div class="bg-white p-3 rounded-md">1</div>
-		<div class="bg-white p-3 rounded-md row-span-2">10</div>
-		<div class="bg-white p-3 rounded-md col-span-2">9</div>
-		<div class="bg-white p-3 rounded-md">9</div>
-		<div class="bg-white p-3 rounded-md">9</div>
+	<div
+		class="flex flex-col gap-5 justify-center items-center bg-gradient-to-r from-sky-500 to-indigo-500 dark:from-slate-400 h-48 sm:h-52 md:h-56 lg:h-60 xl:h-64 2xl:h-72"
+	>
+		<h1
+			class="text-bold text-3xl sm:text-4xl xl:text-5xl font-bold text-gray-100 tracking-wide"
+		>
+			Hey, I'm Peter!
+		</h1>
+		<h4
+			class="text-bold text-sm sm:text-base xl:text-xl font-bold text-gray-100 tracking-wide"
+		>
+			Developer | Always a Student | Hacker
+		</h4>
 	</div>
-	<div class="grid grid-cols-4 gap-3 p-3 bg-gray-200 dark:bg-slate-400">
-		<div class="bg-white p-3 rounded-md col-span-3">1</div>
-		<div class="bg-white p-3 rounded-md">2</div>
-		<div class="bg-white p-3 rounded-md">3</div>
-		<div class="bg-white p-3 rounded-md">4</div>
-		<div class="bg-white p-3 rounded-md">5</div>
-		<div class="bg-white p-3 rounded-md row-span-4">6</div>
-		<div class="bg-white p-3 rounded-md">7</div>
-		<div class="bg-white p-3 rounded-md">8</div>
-		<div class="bg-white p-3 rounded-md">9</div>
-		<div class="bg-white p-3 rounded-md">2</div>
-		<div class="bg-white p-3 rounded-md">1</div>
-		<div class="bg-white p-3 rounded-md row-span-2">10</div>
-		<div class="bg-white p-3 rounded-md col-span-2">9</div>
-		<div class="bg-white p-3 rounded-md">9</div>
-		<div class="bg-white p-3 rounded-md">9</div>
+	<div
+		class="bg-gray-200 bg-opacity-80 dark:bg-slate-400 flex flex-col gap-5 justify-center items-start px-4 py-5 sm:px-8 sm:py-9 md:px-12 md:py-12 lg:px-20 lg:py-14 xl:px-32 xl:py-16 2xl:px-48 2xl:py-20"
+	>
+		<p class="text-bold text-slate-900 text-xl">
+			I'm a passionate software developer who strives for excellent code and
+			eloquent design. I love working on projects in my free time and learning
+			new technologies. I collaborate with many of my friends on various
+			interesting projects to further my software skills and give back to the
+			software community.
+		</p>
+		<p class="text-bold text-slate-900 text-xl">
+			I graduated from the University of Ottawa with a BASc in Computer
+			Engineering in 2020. I have experience in the following fields and
+			technologies:
+		</p>
+		<ul class="text-bold text-slate-900 text-xl">
+			<li>- Programming Languages : Python, JavaScript, Java.</li>
+			<li>
+				- Web Development : React, Vue, Material UI, Bootstrap, TailwindCSS.
+			</li>
+			<li>- Back-end Development: Django, NodeJS, PostgreSQL, Docker.</li>
+		</ul>
+
+		<p class="text-bold text-slate-900 text-xl">
+			I'm looking to further my skills and work with new technologies. I am
+			always open to learning new programming standards and utilize new
+			technologies to provide efficient and clean software.
+		</p>
 	</div>
 </template>
